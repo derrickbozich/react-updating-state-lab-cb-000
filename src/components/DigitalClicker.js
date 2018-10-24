8529eeee33d2de84b7f97b22a80fb1d7500e2701
derrick b <dbozich1@gmail.com>
2018-10-24 03:42:43 +0000

    automatically backed up by learn

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -5,7 +5,7 @@ export default class DigitalClicker extends React.Component{
   constructor(){
     super();
     this.state = {
-      timesClicked = 0
+      timesClicked : 0
     }
 
     handleEvent = event => {
