aab655fc9acb49ffef923a5e3d142fb57c63d40d
derrick b <dbozich1@gmail.com>
2018-10-24 03:33:03 +0000

    automatically backed up by learn

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,11 @@
 // Code DigitalClicker Component Here
+import React from 'react'
+
+export default class DigitalClicker extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      timesClicked = 0
+    }
+  }
+}
